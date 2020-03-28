c743018b939e9dcd417938b30390e69335c0d8b2
MTLY-1610 <larrouy.m@me.com>
2020-03-28 19:03:34 +0100

    none

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -12,37 +12,47 @@ class Payment extends React.Component<Props, State> {
     
     return <div className="payment-layout">
       <div className="Recap">
-      <div className="cart-Recap">
-        <div className="cart-img-info-div">
-            <h5>ITEM:</h5>
-            <h5>QTY:</h5>
-            <h5>SIZE:</h5>
-            <h5>PRICE:</h5>
-        </div>
-          <ShoppingCartConsumer>
-            {value => {
-              return value.shoppingCart.map(item => {
-                return (
-                  <React.Fragment>
-                    <div className="cart-img-info-div">
-                      <div className="cart-imgdiv">
-                        <img src={item.product.img} alt="img" />
-                      </div>
-                      <div className="qtydiv">{item.count}</div>
-                      <div className="sizediv">{item.size}</div>
-                      <div className="pricediv">{item.product.price}$</div>
-                    </div>
-                  </React.Fragment>
-                );
-              });
-            }}
-          </ShoppingCartConsumer>  
-        <div className="cart-img-info-div">
-          <h5>TOTAL</h5>
-        </div>
+          <div className="cart-Recap">
+            <div className="cart-img-info-div">
+                <h5>ITEM:</h5>
+                <h5>QTY:</h5>
+                <h5>SIZE:</h5>
+                <h5>PRICE:</h5>
+            </div>
+              <ShoppingCartConsumer>
+                {value => {
+                  return value.shoppingCart.map(item => {
+                    return (
+                      <React.Fragment>
+                        <div className="cart-img-info-div">
+                          <div className="cart-imgdiv">
+                            <img src={item.product.img} alt="img" />
+                          </div>
+                          <div className="qtydiv">{item.count}</div>
+                          <div className="sizediv">{item.size}</div>
+                          <div className="pricediv">{item.product.price}$</div>
+                        </div>
+                      </React.Fragment>
+                    );
+                  });
+                }}
+              </ShoppingCartConsumer>  
+            <div className="cart-img-info-div">
+              <h5>TOTAL</h5>
+            </div>
       
+            </div>
+      <div className="adress-Recap"></div>
       </div>
+
+      <div className="Now">
+        <div className="solution">
+          <div className="payment-Solution"></div>
+          <div className="delivery-Solution"></div>
+        </div>
+
       </div>
+
 
 
     </div>;
