49ae0c9f2a91d2f543a5a208b4d0de7c991d24eb
MTLY-1610 <larrouy.m@me.com>
2020-03-28 14:33:22 +0100

    cart-Recap

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -1,12 +1,46 @@
 import * as React from "react";
 
+import { Link } from "react-router-dom";
+import { ShoppingCartConsumer } from "../contexts/CartContext";
+
 export interface Props {}
 
 export interface State {}
 
 class Payment extends React.Component<Props, State> {
   render() {
-    return <div>Welcome to payment!</div>;
+    
+    return <div >
+      <div className="cart-Recap">
+        <div className="cart-Recap-Header">
+            <h5>ITEM:</h5>
+            <h5>QTY:</h5>
+            <h5>SIZE:</h5>
+            <h5>PRICE:</h5>
+        </div>
+      
+          
+            <ShoppingCartConsumer>
+            {value => {
+              return value.shoppingCart.map(item => {
+                return (
+                  <React.Fragment>
+                    <div className="cart-img-info-div">
+                      <div className="cart-imgdiv">
+                        <img src={item.product.img} alt="img" />
+                      </div>
+                      <div className="qtydiv">{item.count}</div>
+                      <div className="sizediv">{item.size}</div>
+                      <div className="pricediv">{item.product.price}$</div>
+                    </div>
+                  </React.Fragment>
+                );
+              });
+            }}
+          </ShoppingCartConsumer>
+      
+      </div>
+    </div>;
   }
 }
 
