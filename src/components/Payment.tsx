070c64ca620027e599d58ef34087996bda198635
MTLY-1610 <larrouy.m@me.com>
2020-03-28 15:01:46 +0100

    some s***t

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -40,6 +40,35 @@ class Payment extends React.Component<Props, State> {
           </ShoppingCartConsumer>
       
       </div>
+      <div className="cart-Recap">
+        <div className="cart-Recap-Header">
+            <h5>ITEM:</h5>
+            <h5>QTY:</h5>
+            <h5>SIZE:</h5>
+            <h5>PRICE:</h5>
+        </div>
+      
+          
+            <ShoppingCartConsumer>
+            {value => {
+              return value.shoppingCart.map(item => {
+                return (
+                  <React.Fragment>
+                    <div className="cart-img-info-div">
+                      <div className="cart-imgdiv">
+                        <img src={item.product.img} alt="img" />
+                      </div>
+                      <div className="qtydiv">{item.count}</div>
+                      <div className="sizediv">{item.size}</div>
+                      <div className="pricediv">{item.product.price}$</div>
+                    </div>
+                  </React.Fragment>
+                );
+              });
+            }}
+          </ShoppingCartConsumer>
+      
+      </div>
     </div>;
   }
 }
