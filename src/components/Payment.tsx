94d8fc3677772898c9ea3ac608a613facc84eac3
MTLY-1610 <larrouy.m@me.com>
2020-03-28 17:05:33 +0100

    layout gen

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -10,17 +10,16 @@ export interface State {}
 class Payment extends React.Component<Props, State> {
   render() {
     
-    return <div >
+    return <div className="payment-layout">
+      <div className="Recap">
       <div className="cart-Recap">
-        <div className="cart-Recap-Header">
+        <div className="cart-img-info-div">
             <h5>ITEM:</h5>
             <h5>QTY:</h5>
             <h5>SIZE:</h5>
             <h5>PRICE:</h5>
         </div>
-      
-          
-            <ShoppingCartConsumer>
+          <ShoppingCartConsumer>
             {value => {
               return value.shoppingCart.map(item => {
                 return (
@@ -37,38 +36,15 @@ class Payment extends React.Component<Props, State> {
                 );
               });
             }}
-          </ShoppingCartConsumer>
-      
-      </div>
-      <div className="cart-Recap">
-        <div className="cart-Recap-Header">
-            <h5>ITEM:</h5>
-            <h5>QTY:</h5>
-            <h5>SIZE:</h5>
-            <h5>PRICE:</h5>
+          </ShoppingCartConsumer>  
+        <div className="cart-img-info-div">
+          <h5>TOTAL</h5>
         </div>
       
-          
-            <ShoppingCartConsumer>
-            {value => {
-              return value.shoppingCart.map(item => {
-                return (
-                  <React.Fragment>
-                    <div className="cart-img-info-div">
-                      <div className="cart-imgdiv">
-                        <img src={item.product.img} alt="img" />
-                      </div>
-                      <div className="qtydiv">{item.count}</div>
-                      <div className="sizediv">{item.size}</div>
-                      <div className="pricediv">{item.product.price}$</div>
-                    </div>
-                  </React.Fragment>
-                );
-              });
-            }}
-          </ShoppingCartConsumer>
-      
       </div>
+      </div>
+
+
     </div>;
   }
 }
