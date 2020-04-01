b7c6eebb4a8abe56a844fe31406113e9bcf9420c
Mattin <mattin-najafi@hotmail.com>
2020-04-01 12:03:08 +0200

    messed up the merge conflict but fixed it

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -3,158 +3,97 @@ import CreditCardIcon from "@material-ui/icons/CreditCard";
 import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
 import { Link } from "react-router-dom";
 import { ShoppingCartConsumer } from "../contexts/CartContext";
-import purple from "@material-ui/core/colors/purple";
-import red from "@material-ui/core/colors/red";
 
 export interface Props {}
 
 export interface State {}
-
 class Payment extends React.Component<Props, State> {
   render() {
-
-
-    
-    return <div className="payment-layout">
-      <div className="Recap">
+    return (
+      <div className="payment-layout">
+        <div className="Recap">
           <div className="cart-Recap">
             <div className="cart-img-info-div">
-                <h5>ITEM:</h5>
-                <h5>QTY:</h5>
-                <h5>SIZE:</h5>
-                <h5>PRICE:</h5>
+              <h5>ITEM:</h5>
+              <h5>QTY:</h5>
+              <h5>SIZE:</h5>
+              <h5>PRICE:</h5>
             </div>
-              <ShoppingCartConsumer>
-                {value => {
-                  return value.shoppingCart.map(item => {
-                    return (
-                      <React.Fragment>
-                        <div className="cart-img-info-div">
-                          <div className="cart-imgdiv">
-                            <img src={item.product.img} alt="img" />
-                          </div>
-                          <div className="qtydiv">{item.count}</div>
-                          <div className="sizediv">{item.size}</div>
-                          <div className="pricediv">{item.product.price}$</div>
+            <ShoppingCartConsumer>
+              {value => {
+                return value.shoppingCart.map(item => {
+                  return (
+                    <React.Fragment>
+                      <div className="cart-img-info-div">
+                        <div className="cart-imgdiv">
+                          <img src={item.product.img} alt="img" />
                         </div>
-                      </React.Fragment>
-                    );
-                  });
-                }}
-              </ShoppingCartConsumer>  
-            <div className="cart-img-info-div">
+                        <div className="qtydiv">{item.count}</div>
+                        <div className="sizediv">{item.size}</div>
+                        <div className="pricediv">{item.product.price}$</div>
+                      </div>
+                    </React.Fragment>
+                  );
+                });
+              }}
+            </ShoppingCartConsumer>
+            <div className="headers">
               <h5>TOTAL</h5>
             </div>
-          
-      
-      </div>
-      <div className="adress-Recap">
-      <h5 className="cart-img-info-div">DELIVERY ADRESS</h5>
-      </div>
-      </div>
-
-      <div className="Now">
-        <div className="solution">
-          <div className="payment-Solution"><h5 className="cart-img-info-div">PAYMENT METHOD</h5><CreditCardIcon/><AccountBalanceIcon/></div>
-          <div className="delivery-Solution"><h5 className="cart-img-info-div">DELIVERY OPTIONS</h5></div>
+          </div>
+          <div className="adress-Recap">
+            <h5 className="headers">DELIVERY ADRESS</h5>
+            <div className="firstname">
+              <span>FIRSTNAME:</span>
+              <input type="text" />
+            </div>
+            <div className="lastname">
+              <span>LASTNAME:</span>
+              <input type="text" />
+            </div>
+            <div className="adress">
+              <span>ADRESS:</span>
+              <input type="text" />
+            </div>
+            <div className="adress">
+              <span>EMAIL:</span>
+              <input type="text" />
+            </div>
+            <div className="country">
+              <span>COUNTRY:</span>
+              <input type="text" />
+            </div>
+            <div className="zip">
+              <span>ZIPCODE:</span>
+              <input type="text" />
+            </div>
+             
+          </div>
         </div>
-      </div>
-      <div className="payment-chosen-pay">
-      <h5 className="cart-img-info-div"> </h5>
-      </div>
-      
-      <div className="paymentbutton">
-            <Link to="/Checkout">
-              <button>CONFIRM PAYMENT</button>
-            </Link>
-
-      
-</div>
-</div>
-
-
-          <div className="payment-layout">
-            <div className="Recap">
-              <div className="cart-Recap">
-                <div className="cart-img-info-div">
-                  <h5>ITEM:</h5>
-                  <h5>QTY:</h5>
-                  <h5>SIZE:</h5>
-                  <h5>PRICE:</h5>
-                </div>
-                <ShoppingCartConsumer>
-                  {value => {
-                    return value.shoppingCart.map(item => {
-                      return (
-                        <React.Fragment>
-                          <div className="cart-img-info-div">
-                            <div className="cart-imgdiv">
-                              <img src={item.product.img} alt="img" />
-                            </div>
-                            <div className="qtydiv">{item.count}</div>
-                            <div className="sizediv">{item.size}</div>
-                            <div className="pricediv">
-                              {item.product.price}$
-                            </div>
-                          </div>
-                          <div className="personal-button">
-                            <div className="personalinfo-div">
-                              <div className="namediv">
-                                <div className="firstname">
-                                  <span>FIRSTNAME:</span>
-                                  <input type="text" />
-                                </div>
-                                <div className="lastname">
-                                  <span>LASTNAME:</span>
-                                  <input type="text" />
-                                </div>
-                              </div>
-                              <div className="adress">
-                                <span>ADRESS:</span>
-                                <input type="text" />
-                              </div>
-                              <div className="adress">
-                                <span>EMAIL:</span>
-                                <input type="text" />
-                              </div>
-                              <div className="countryandzip">
-                                <div className="country">
-                                  <span>COUNTRY:</span>
-                                  <input type="text" />
-                                </div>
-                                <div className="zip">
-                                  <span>ZIPCODE:</span>
-                                  <input type="text" />
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </React.Fragment>
-                      );
-                    });
-                  }}
-                </ShoppingCartConsumer>
-                <div className="cart-img-info-div">
-                  <h5>TOTAL</h5>
-                </div>
-              </div>
+         
+        <div className="Now">
+          <div className="solution">
+            <div className="payment-Solution">
+              <h5 className="headers">PAYMENT METHOD</h5>
+              <CreditCardIcon />
+              <AccountBalanceIcon />
+            </div>
+            <div className="delivery-Solution">
+              <h5 className="headers">DELIVERY OPTIONS</h5>
             </div>
           </div>
-
-          <div className="Now"></div>
-          <div className="payment-chosen-pay">
-            <h5 className="cart-img-info-div"> </h5>
-          </div>
-
-          <div className="paymentbutton">
-            <Link to="/Checkout">
-              <button>CONFIRM PAYMENT</button>
-            </Link>
-          </div>
+        </div>
+        <div className="payment-chosen-pay">
+          <h5 className="headers"> </h5>
+        </div>
+        <div className="paymentbutton">
+          <Link to="/Checkout">
+            <button>CONFIRM PAYMENT</button>
+          </Link>
+           
         </div>
       </div>
     );
   }
 }
-
 export default Payment;
