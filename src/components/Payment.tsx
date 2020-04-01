ebd0124b74e060dd1dfdaa364353bc877e0ca31f
MTLY-1610 <larrouy.m@me.com>
2020-04-01 13:32:57 +0200

    basic layout payment

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -3,10 +3,16 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCartConsumer } from "../contexts/CartContext";
 import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+
+
 
 export interface Props {}
 
 export interface State {}
+
+
+
 class Payment extends React.Component<Props, State> {
   render() {
     return (
@@ -41,6 +47,61 @@ class Payment extends React.Component<Props, State> {
               <h5>TOTAL</h5>
             </div>
           </div>
+          
+        </div>
+         
+        <div className="Now">
+          <div className="solution">
+            <div className="payment-Solution">
+              <h5 className="headers">PAYMENT METHOD</h5>
+              <div className="Choices">
+              <Checkbox /> <h5 className="headers">Credit Card</h5>
+              
+              <Checkbox /> <h5 className="headers">Bank Transfer</h5>
+              
+             </div>
+            </div>
+            <div className="delivery-Solution">
+              <h5 className="headers">DELIVERY</h5>
+              <div className="Choices">
+              <Checkbox /> <h5 className="headers">EXPRESS</h5>
+              
+              <Checkbox /> <h5 className="headers">REGULAR</h5>
+              
+             </div>
+            </div>
+            
+          </div>
+        </div>
+        <div className="pay">
+        <div className="adress-Recap">
+            <h5 className="headers">DELIVERY ADRESS</h5>
+            <div className="firstname">
+              <span>FIRSTNAME:</span>
+              <input type="text" />
+            </div>
+            <div className="lastname">
+              <span>LASTNAME:</span>
+              <input type="text" />
+            </div>
+            <div className="adress">
+              <span>ADRESS:</span>
+              <input type="text" />
+            </div>
+            <div className="adress">
+              <span>EMAIL:</span>
+              <input type="text" />
+            </div>
+            <div className="country">
+              <span>COUNTRY:</span>
+              <input type="text" />
+            </div>
+            <div className="zip">
+              <span>ZIPCODE:</span>
+              <input type="text" />
+            </div>
+             
+          </div>
           <div className="adress-Recap">
             <h5 className="headers">DELIVERY ADRESS</h5>
             <div className="firstname">
@@ -70,35 +131,12 @@ class Payment extends React.Component<Props, State> {
              
           </div>
         </div>
-         
-        <div className="Now">
-          <div className="solution">
-            <div className="payment-Solution">
-              <h5 className="headers">PAYMENT METHOD</h5>
-              <div className="Choices">
-              <Checkbox /> <h5 className="headers">Credit Card</h5>
-              
-              <Checkbox /> <h5 className="headers">Bank Transfer</h5>
-              
-             </div>
-            </div>
-            <div className="payment-Solution">
-              <h5 className="headers">PAYMENT METHOD</h5>
-              <div className="Choices">
-              <Checkbox /> <h5 className="headers">Credit Card</h5>
-              
-              <Checkbox /> <h5 className="headers">Bank Transfer</h5>
-              
-             </div>
-            </div>
-          </div>
-        </div>
-        <div className="payment-chosen-pay">
-          <h5 className="headers"> </h5>
-        </div>
+        
+          
+        
         <div className="paymentbutton">
           <Link to="/Checkout">
-            <button>CONFIRM PAYMENT</button>
+          <Button>PAY</Button>
           </Link>
            
         </div>
